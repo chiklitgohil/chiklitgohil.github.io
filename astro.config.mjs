d9a9753f43bae0531816785a3c708528435d9f2f
chiklitgohil <chiklitgohil01@gmail.com>
2026-09-19 02:06:35 +0530

    Replace Digital Garden workflow with simple redirect

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,9 @@ import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
   site: "https://chiklitgohil.github.io",
+  redirects: {
+    '/garden': 'https://chiklit-knowledge-base.vercel.app/'
+  },
   integrations: [
     tailwind({
       applyBaseStyles: false,
